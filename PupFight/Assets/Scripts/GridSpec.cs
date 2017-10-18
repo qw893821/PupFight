faa210f33609f4099542a397f2274948b3b6483b
qw893821 <xiuman893821@gmail.com>
2017-10-18 14:19:07 -0400

    grid prefabe
    grid prefabe is created. the most basic

---
+++ PupFight/Assets/Scripts/GridSpec.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSpec: MonoBehaviour {
+    Ray camRay;
+    RaycastHit hit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(camRay, out hit))
+        {
+            print("1");
+            if (hit.collider.tag == "HighLight")
+            {
+                hit.collider.gameObject.GetComponent<Renderer>().enabled = true;
+                print("hit");
+            }
+            
+        }
+        else
+        {
+           transform.Find("Interactable").gameObject.GetComponent<Renderer>().enabled = false;
+        }
+    }
+}
