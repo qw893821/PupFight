615c96946de2dec22de6b3e322773d39d4e83906
qw893821 <xiuman893821@gmail.com>
2017-11-11 20:24:59 -0500

    ready to add teammanager

---
+++ PupFight/Assets/Scripts/FriendManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendManager{
+    static GameObject[] friends;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    static void GetPlayerTeam()
+    {
+        friends = GameObject.FindGameObjectsWithTag("Player");
+        
+    }
+
+    static void RemovePlayer()
+    {
+        for(int i = 0; i < friends.Length; i++)
+        {
+            //if(friends[i])
+        }
+    }
+}
